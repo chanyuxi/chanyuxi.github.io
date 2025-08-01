5e3b9ae620f3dd9c6acc73f44a06a49205904506
Chan-Yuxi <2438149743@qq.com>
2025-08-01 20:21:01 +0800

    chore(eslint): enable react

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,9 @@
 import waltz from '@chanyuxi/eslint-waltz'
 
 export default waltz({
+  gitignore: true,
   js: { globals: ['browser'] },
   ts: true,
-  gitignore: true,
+  imports: true,
+  react: true,
 })
